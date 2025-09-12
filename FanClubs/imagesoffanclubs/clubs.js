79cb03228a6eaa5c5a59ba389b46bc18f8ac669d
Robert <152218269+PenguinRobert1@users.noreply.github.com>
2025-09-12 18:06:17 +0000

    this fine

---
+++ FanClubs/imagesoffanclubs/clubs.js
@@ -0,0 +1,6 @@
+(function() {
+  const galaxyElement = document.getElementById('galaxy');
+  galaxyElement.style.position = 'absolute';
+  galaxyElement.style.top = '0px';
+  galaxyElement.style.left = '0px';
+})();
